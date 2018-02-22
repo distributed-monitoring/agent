a6f1cfa29e2ca86e022ac5b44148b22e61a41dec
Toshiaki Takahashi <takahashi.tsc@ncos.nec.co.jp>
2018-02-22 07:47:37 +0000

    Add debug logging

--- agent.go
+++ agent.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"os/exec"
 	"strings"
@@ -14,28 +15,36 @@ var (
 func main() {
 	outStatus, errStatus := exec.Command("sudo", "systemctl", "status", "collectd").Output()
 	if errStatus != nil {
+		fmt.Printf("status NG\n")
 		os.Exit(1)
 	}
 	if !strings.Contains(string(outStatus), "running") {
+		fmt.Printf("status not running\n")
 		os.Exit(1)
 	}
 
 	_, errStop := exec.Command("sudo", "systemctl", "stop", "collectd").Output()
 	if errStop != nil {
+		fmt.Printf("stop NG\n")
 		os.Exit(1)
 	}
 
 	flag.Parse()
 	if _, errFile := os.Stat(*collectdConfig); errFile != nil {
+		fmt.Printf("file not found\n")
 		os.Exit(1)
 	}
-	errCp := exec.Command("sudo", "cp", *collectdConfig, "/etc/collectd/collectd.conf").Run()
+	errCp := exec.Command("sudo", "cp", *collectdConfig, "/etc/collectd/collectd.conf.d/localagent.conf").Run()
 	if errCp != nil {
+		fmt.Printf("cp NG\n")
 		os.Exit(1)
 	}
 
 	_, errStart := exec.Command("sudo", "systemctl", "start", "collectd").Output()
 	if errStart != nil {
+		fmt.Printf("start NG\n")
 		os.Exit(1)
 	}
+
+	fmt.Printf("All complete!\n")
 }
